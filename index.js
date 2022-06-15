6bf997926e9b400b357113de913273c603074b51
kevin breid <kevinbreid@yahoo.com>
2022-06-15 14:19:38 -0700

    commit tree 045831714cb64490c580b50505ccf6cb9e162a4a

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+
+function destructivelyAppendCat(){
+
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat(){
+
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+
+    cats.shift()
+}
+
+function appendCat(){
+
+    const catsNew = [...cats, 'Broom']
+
+    return catsNew
+}
+
+function prependCat(){
+
+    let catsNewTwo = ['Arnold', ...cats]
+
+    return catsNewTwo
+}
+
+function removeLastCat(){
+
+    const removeLastCat = cats.slice(0,2)
+    return removeLastCat
+}
+
+function removeFirstCat(){
+
+    const removeFirstCat = cats.slice(1,3)
+
+    return removeFirstCat
+}
